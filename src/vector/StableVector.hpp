9c83df724cff171f355ed9c002a7eae05a96ee42
Mixmp29 <denisov.mihail2013@yandex.ru>
2022-05-16 13:08:11 +0700

    Prepared project build

---
+++ src/vector/StableVector.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+
+class TimeUnit {};
---
+++ src/vector/VectorTest.cpp
@@ -0,0 +1,10 @@
+#include <vector/StableVector.hpp>
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+TEST(TimeSuite, Test)
+{
+    EXPECT_EQ(42 + 1, 43);
+}
