60ae3f51245fd8823101b98e9cab746eaf699c6e
Mixmp29 <denisov.mihail2013@yandex.ru>
2022-05-18 09:10:24 +0700

    Declare functions

--- src/vector/StableVector.hpp
+++ src/vector/StableVector.hpp
@@ -13,6 +13,12 @@ public:
     Vec& operator=(Vec&&) noexcept;
     ~Vec() noexcept;
 
+    Vec& operator=(std::initializer_list<T>);
+    void push_back(const T&);
+    void push_back(T&&);
+    template <class... Args>
+    void emplace_back(Args&&...);
+
 private:
     T* elements = nullptr;
     T* first_free = nullptr;
